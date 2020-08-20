35f3dd2d725c28eb0e7d9295e3ffdfa3ccb7583d
Ning1994 <765211630@qq.com>
2020-08-20 18:42:12 +0800

    first commit

--- src/main/kotlin/org/ning1994/net_assist/MainStyles.kt
+++ src/main/kotlin/org/ning1994/net_assist/MainStyles.kt
@@ -1,14 +1,13 @@
 package org.ning1994.net_assist
 
+import javafx.scene.paint.Color
 import javafx.scene.text.FontWeight
-import tornadofx.Stylesheet
-import tornadofx.box
-import tornadofx.cssclass
-import tornadofx.px
+import tornadofx.*
 
 class MainStyles : Stylesheet() {
     companion object {
         val heading by cssclass()
+        val formBlockPanel by cssclass()
     }
 
     init {
@@ -16,6 +15,22 @@ class MainStyles : Stylesheet() {
             padding = box(10.px)
             fontSize = 20.px
             fontWeight = FontWeight.BOLD
+        }
+        text {
+            fontSize = 12.px
+        }
+        title {
+            fontSize = 12.px
+        }
+        choiceBox {
+            fontSize = 12.px
+        }
+        fieldset {
+            fontSize = 12.px
+        }
+        formBlockPanel{
+            backgroundColor += Color.AQUA
+            padding = box(8.0.px)
         }
     }
 }
--- src/main/kotlin/org/ning1994/net_assist/MainView.kt
+++ src/main/kotlin/org/ning1994/net_assist/MainView.kt
@@ -7,7 +7,6 @@ import javafx.beans.property.SimpleObjectProperty
 import javafx.beans.property.SimpleStringProperty
 import javafx.geometry.Orientation
 import javafx.geometry.Pos
-import javafx.scene.paint.Color
 import javafx.util.StringConverter
 import org.ning1994.net_assist.core.SocketProtocol
 import tornadofx.*
@@ -18,7 +17,8 @@ class MainView : View("NetAssist", NetAssist.loadIcon()) {
     private val socketProtocol = SimpleObjectProperty<SocketProtocol>(socketProtocolList[0])
     private val ip = SimpleStringProperty("127.0.0.1")
     private val port = SimpleIntegerProperty(8888)
-
+    private val sendNewlineTypes = arrayListOf("\\n", "\\r\\n", "\\r", "无")
+    private val sendNewlineType = SimpleStringProperty(sendNewlineTypes[0])
 
     override val root = vbox {
         borderpane {
@@ -26,62 +26,98 @@ class MainView : View("NetAssist", NetAssist.loadIcon()) {
                 form {
                     fieldset("网络设置") {
                         field(orientation = Orientation.VERTICAL) {
-                            style {
-                                backgroundColor += Color.AQUA
-                                paddingAll = 8
-                            }
-                            fieldset("协议类型") {
-                                choicebox(socketProtocol, socketProtocolList) {
-                                    this.converter = object : StringConverter<SocketProtocol>() {
+                            addClass(MainStyles.formBlockPanel)
+                            label("协议类型")
+                            choicebox(socketProtocol, socketProtocolList) {
+                                this.converter = object : StringConverter<SocketProtocol>() {
 
-                                        override fun toString(`object`: SocketProtocol?): String = `object`?.displayName
-                                            ?: ""
+                                    override fun toString(`object`: SocketProtocol?): String = `object`?.displayName
+                                        ?: ""
 
-                                        override fun fromString(string: String?): SocketProtocol =
-                                            socketProtocolList.find { it.displayName == string }!!
-                                    }
+                                    override fun fromString(string: String?): SocketProtocol =
+                                        socketProtocolList.find { it.displayName == string }!!
                                 }
                             }
-                            fieldset("远程IP地址") {
-                                textfield(ip) {
-
-                                }
-                            }
-                            fieldset("远程端口号") {
-                                textfield(port) {
-
-                                }
+                            label("远程IP地址")
+                            textfield(ip)
+                            label("远程端口号")
+                            textfield(port)
+                            hbox(8) {
+                                button("开始监听")
+                                button("断开")
                             }
                         }
-
                     }
-
+                    fieldset("接收设置") {
+                        field(orientation = Orientation.VERTICAL) {
+                            addClass(MainStyles.formBlockPanel)
+                            checkbox("保存数据到文件")
+                            checkbox("自动换行显示")
+                            checkbox("显示接收时间")
+                            checkbox("16进制显示")
+                            checkbox("暂停显示")
+                            hbox(8) {
+                                button("保存数据")
+                                button("清空显示")
+                            }
+                        }
+                    }
+                    fieldset("发送设置") {
+                        field(orientation = Orientation.VERTICAL) {
+                            addClass(MainStyles.formBlockPanel)
+                            hbox(8) {
+                                label("换行符")
+                                choicebox(sendNewlineType, sendNewlineTypes)
+                            }
+                            checkbox("载入文件")
+                            checkbox("自动发送附加位")
+                            checkbox("发送完自动清空")
+                            checkbox("16进制发送")
+                            hbox(8) {
+                                checkbox("周期发送")
+                                textfield()
+                                label("ms")
+                            }
+                            hbox(8) {
+                                button("发送文件")
+                                button("清空显示")
+                            }
+                        }
+                    }
                 }
             }
-            right {
+            center {
+                form {
+                    fieldset("数据接收") {
+                        field(orientation = Orientation.VERTICAL) {
+                            addClass(MainStyles.formBlockPanel)
+                            textarea {  }
 
+                        }
+                    }
+                    fieldset("数据发送") {
+                        field {
+                            addClass(MainStyles.formBlockPanel)
+                            textarea {  }
+                            button("数据发送")
+                        }
+                    }
+                }
             }
             bottom {
-
-            }
-        }
-        paddingLeft = 15
-        paddingRight = 15
-        paddingTop = 10
-        paddingBottom = 10
-        alignment = Pos.CENTER
-        label("正在加载...") {
-            paddingBottom = 5
-        }
-        progressbar(progressValue) {
-            minWidth = 400.0
-            progressProperty().animate(1, 0.5.seconds, Interpolator.LINEAR) {
-                setOnFinished {
-//                    close()
-//                    MainView().openWindow()
+                hbox(8) {
+                    addClass(MainStyles.formBlockPanel)
+                    label("正在加载...") {
+                    }
+                    progressbar(progressValue) {
+                        minWidth = 400.0
+                        progressProperty().animate(1, 0.5.seconds, Interpolator.LINEAR) {
+                            setOnFinished {
+                            }
+                        }
+                    }
                 }
             }
-
         }
     }
 
